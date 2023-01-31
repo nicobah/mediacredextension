d7d799cb75e964b146d48c02ace8f5a3420b9753
nicobah <42782542+nicobah@users.noreply.github.com>
2023-01-31 09:37:51 +0100

    extension

---
+++ extension/scripts/contents.js
@@ -0,0 +1,7 @@
+var domain = "";
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    document.getElementById('domain').innerHTML = url;
+
+    // use `url` here inside the callback because it's asynchronous!
+});
