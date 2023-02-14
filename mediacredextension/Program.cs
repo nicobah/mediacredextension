df4b1cd864806bceeb6cf649945afad2a7aea118
nicobah <42782542+nicobah@users.noreply.github.com>
2023-02-14 13:16:18 +0100

    mudblazor setup

---
+++ mediacredextension/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace mediacredextension
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
+
+            builder.Services.AddBrowserExtensionServices();
+            await builder.Build().RunAsync();
+        }
+    }
+}
