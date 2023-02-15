9203c86b75e45fda6e5754059602b0af047eeab6
nicobah <42782542+nicobah@users.noreply.github.com>
2023-02-15 08:11:23 +0100

    popup created

--- mediacredextension/Program.cs
+++ mediacredextension/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 namespace mediacredextension
 {
@@ -15,6 +16,7 @@ namespace mediacredextension
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddBrowserExtensionServices();
