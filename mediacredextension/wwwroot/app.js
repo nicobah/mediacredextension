fe08f85fd8e2323bf0b941518cc67af71183628b
nicobah <42782542+nicobah@users.noreply.github.com>
2023-03-16 22:57:31 +0100

    Delete mediacredextension directory

--- mediacredextension/wwwroot/app.js
+++
@@ -1,5 +0,0 @@
-﻿if (globalThis.BlazorBrowserExtension.BrowserExtension.Mode === globalThis.BlazorBrowserExtension.Modes.ContentScript) {
-    const appDiv = document.createElement("div");
-    appDiv.id = "kand_extension_uid";
-    document.body.appendChild(appDiv);
-}
